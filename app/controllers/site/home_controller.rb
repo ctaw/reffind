bd91177b7be5c5c4085a0d94ed96d1e0749fe88b
Charlene Lopez Taw <tawcharlene@gmail.com>
2016-05-27 18:02:14 +0800

    add message when search is empty

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,23 +1,6 @@
 require 'flickraw'
 require 'will_paginate/array'
 
-=begin
-Extended FlickRaw class to implement some helper methods for use in the view.
-
-Flickr image size suffix notation:
-
-s	small square 75x75
-q	large square 150x150
-t	thumbnail, 100 on longest side
-m	small, 240 on longest side
-n	small, 320 on longest side
--	medium, 500 on longest side
-z	medium 640, 640 on longest side
-c	medium 800, 800 on longest side
-b	large, 1024 on longest side
-h	large 1600, 1600 on longest side
-k	large 2048, 2048 on longest side
-=end
 class FlickRaw::Response
   def url_q
     build_img_url.gsub('?', 'q')
@@ -32,7 +15,7 @@ class FlickRaw::Response
   end
 end
 
-# Main controller for the application. Handles searches and results.
+# Main controller for the application
 class Site::HomeController < SiteController
   # Home page with no results
   def index
@@ -43,28 +26,29 @@ class Site::HomeController < SiteController
   def search
     @results = []
 
-    # Searching nothing throws an exception with the gem
+    # Searching nothing
     if params[:search_photo_text].to_s.strip.empty?
-      # Once-off error message -- need to use .now
-      flash.now[:error_msg] = "Please enter some text to search for..."
+      # Message for no result
+      flash.now[:error_msg] = "Please input correct words to search for... Thank you!"
     else
-      # Cache results for faster pagination
+      # Cache results
+      # Purpose: For Faster Pagination
       @results = Rails.cache.fetch(params[:search_photo_text], :expires_in => AppConfig.cache_expiry_minutes.minutes,
                                    :race_condition_ttl => AppConfig.cache_race_ttl_minutes.minutes) do
 
         logger.info "Caching Flicker API results for: #{params[:search_photo_text]}"
 
-        # Only need to initialise when calling the API
+        # When Calling API only
         FlickRaw.api_key = Rails.application.secrets.flickr_api_key
         FlickRaw.shared_secret = Rails.application.secrets.flickr_shared_secret
 
-        # API call results from Flickr then add to an Array so we can paginate
+        # API results add elements to an Array
         flickr.photos.search(:tags => params[:search_photo_text], :per_page => AppConfig.flickr_max_results).each do |a|
           @results << a
         end
       end
 
-      # Paginate and make instance variable available in the view
+      # Pagination
       @results = @results.paginate(:page => params[:page], :per_page => AppConfig.results_per_page)
     end
   end
